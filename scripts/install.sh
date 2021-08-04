c9181f7e8e4ae9a4c8c1d35d7746134e310547ee
Dan Gittik <dan@sfera.institute>
2021-08-04 12:40:35 +0300

    Added installation script.

---
+++ scripts/install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Stop on errors.
+set -e
+
+# Fix root directory.
+cd "$(dirname $(readlink -f "${BASH_SOURCE[0]}"))/.."
+
+
+NAME="sfera"
+VENV=".env"
+
+
+main() {
+    install-dependencies
+    create-virtual-environment
+    echo "done"
+}
+
+
+install-dependencies() {
+    echo "installing dependencies..."
+    sudo apt-get install -y \
+        build-essential \
+        coreutils \
+        findutils \
+        git \
+        python3
+}
+
+
+create-virtual-environment() {
+    echo "creating virtual environment..."
+    if [ -d "$VENV" ]
+    then
+        echo "virtual environment $VENV already exists"
+        return
+    fi
+
+    # Create virtual environment with a user-friendly prompt.
+    python3 -m venv "$VENV" --prompt="$NAME"
+
+    # Install requirements.
+    "$VENV/bin/pip" install -U pip
+    "$VENV/bin/pip" install -r requirements.txt
+
+    # Make package importable from anywhere.
+    find . -name site-packages -exec bash -c 'echo "$(realpath --relative-to="{}" "$PWD")" > "{}/self.pth"' \;
+}
+
+
+main "$@"
